224712773b2e4a90d8e93d9aa31b50dd255479b0
jlthornton3 <91161036+jlthornton3@users.noreply.github.com>
2022-03-21 20:16:53 -1000

    fix output

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -20,7 +20,7 @@
 
     if($num > 0){
         $quote_arr = array();
-        $quote_arr['data'] = array();
+       // $quote_arr['data'] = array();
 
         while($row = $result->fetch(PDO::FETCH_ASSOC)) {
             extract($row);
